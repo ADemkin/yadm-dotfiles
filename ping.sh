705b9464aa32fc90d5505d7e24fd78676d16009e
Anton Demkin <avdemkin@avito.ru>
2023-10-13 00:10:51 +0400

    Allwo to run gping in new TMUX window with Alt+G

--- ping.sh
+++ ping.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -e
 
 host="ya.ru"
-count=1
+count=5
 
 ping_result="$(ping -c "$count" "$host" 2>/dev/null | tail -1)"
+# echo $ping_result >> debug-ping.log
 ping_ms="$(echo $ping_result | cut -d "=" -f2 | cut -d "/" -f2 | xargs | cut -d "." -f1)"
+# echo $ping_ms >> debug-ping.log
 if [ -z $ping_ms ]; then
     echo "offline"
 else

3b60c6c175b83a2f18e7d302c32ab8326d87dc8a
Anton Demkin <avdemkin@avito.ru>
2023-10-13 21:36:41 +0400

    Update TMUX statusbar scripts

--- battery.sh
+++ battery.sh
@@ -1,26 +1,33 @@
 #!/bin/sh -e
 
-BATT="$(pmset -g batt)"
-PERCENTAGE="$(echo $BATT | grep -o '\d*%')"
+main() {
+    local battery_status="$(pmset -g batt)"
+    local battery_percentage="$(echo $battery_status | grep -o '\d*%')"
 
-if [ -z "$PERCENTAGE" ]; then  # length is zero
-    echo "AC"
-else
-    if [ -n "$(echo $BATT | grep -o 'discharging')" ]; then  # length is nonzero
-        if [ -n "$(echo $BATT | grep -o '(no estimate)')" ]; then
-            # not yet calculated remaining time
-            echo $PERCENTAGE "calculating"
-        else
-            # remaining time calculated
-            echo $PERCENTAGE "$(echo $BATT | grep -oE '\d+:\d+')"
-        fi
+    local status_calculatin="calculating"
+    local status_charged="charged"
+    local status_charging="charging"
+
+    if [ -z "$battery_percentage" ]; then  # length is zero
+        echo "AC"
     else
-        if [ "$PERCENTAGE" = "100%" ]; then
-            echo $PERCENTAGE "charged"
+        if [ -n "$(echo $battery_status | grep -o 'discharging')" ]; then  # length is nonzero
+            if [ -n "$(echo $battery_status | grep -o '(no estimate)')" ]; then
+                # not yet calculated remaining time
+                echo "$battery_percentage $status_calculating"
+            else
+                # remaining time calculated
+                echo $battery_percentage "$(echo $battery_status | grep -oE '\d+:\d+')"
+            fi
         else
-            # WTF?
-            # not yet switched to charging?
-            echo $PERCENTAGE "charging"
+            if [ "$battery_percentage" = "100%" ]; then
+                echo "$battery_percentage $status_charged"
+            else
+                # WTF?
+                # not yet switched to charging?
+                echo "$battery_percentage $status_charging"
+            fi
         fi
     fi
-fi
+}
+main
--- ping.sh
+++ ping.sh
@@ -1,14 +1,24 @@
 #!/bin/sh -e
 
-host="ya.ru"
-count=5
+main() {
+    local ping_host="ya.ru"
+    local ping_count=5
 
-ping_result="$(ping -c "$count" "$host" 2>/dev/null | tail -1)"
-# echo $ping_result >> debug-ping.log
-ping_ms="$(echo $ping_result | cut -d "=" -f2 | cut -d "/" -f2 | xargs | cut -d "." -f1)"
-# echo $ping_ms >> debug-ping.log
-if [ -z $ping_ms ]; then
-    echo "offline"
-else
-    echo "$ping_ms ms"
-fi
+    get_ping() {
+        local ping_result="$(ping -c "$ping_count" "$ping_host" 2>/dev/null | tail -1)"
+        # echo "result: $ping_result" >> debug-ping.log
+        if [ -n "$(grep -o '0 packets received')" ]; then
+            echo "lost"
+        else
+            local ping_ms="$(echo $ping_result | grep -oE "\d{2}\.\d{3}" | sed -n "2p" | cut -d"." -f1)"
+            # echo "ms: $ping_ms" >> debug-ping.log
+            if [ -z $ping_ms ]; then
+                echo "offline"
+            else
+                echo "$ping_ms ms"
+            fi
+        fi
+    }
+    get_ping
+}
+main

26f3f6c50b2571dd7fd3cae8aa88991301a220f5
Anton Demkin <antondemkin@yandex.ru>
2025-08-01 19:52:15 +0200

    PYTHON: Update pythonstartup

--- .pythonstartup.py
+++ .pythonstartup.py
@@ -1,29 +1,6 @@
+import readline
+import rlcompleter
 
-try:
-    import gnureadline as readline
-except ImportError:
-    # print("could not import gnureadline")
-    try:
-        import readline
-    except ImportError:
-        # print("could not import readline")
-        pass
-    pass
-
-try:
-    import rlcompleter
-except ImportError:
-    # print("could not import rlcompleter")
-    pass
-
-autocompletion = globals()
-autocompletion.update(locals())
-completer = rlcompleter.Completer(autocompletion)
+completer = rlcompleter.Completer(globals() | locals())
 readline.parse_and_bind("tab: complete")
 readline.set_completer(completer.complete)
-
-from pprint import pprint as pp
-from datetime import datetime
-from datetime import timedelta 
-from pathlib import Path
-import typing

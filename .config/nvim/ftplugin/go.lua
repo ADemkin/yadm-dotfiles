f00b6ea6f08ca8c0954f8b2194aebbfd4c5349dd
Anton Demkin <antondemkin@yandex.ru>
2025-08-04 15:18:49 +0200

    NVIM: Add ftplugins for lua and go

---
+++ .config/nvim/ftplugin/go.lua
@@ -0,0 +1,6 @@
+-- autocmd FileType go setlocal tabstop=4 shiftwidth=4 softtabstop=0 nolist noexpandtab
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 0
+vim.opt_local.list = false
+vim.opt_local.expandtab = false

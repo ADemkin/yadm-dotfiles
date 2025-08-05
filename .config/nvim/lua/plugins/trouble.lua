0204af6957633f2d71e4439bdb0daf4439ceb3cf
Anton Demkin <antondemkin@yandex.ru>
2025-08-05 16:12:59 +0200

    NVIM: Do not use Trouble for code navigation

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,18 +1,12 @@
 return {
   'folke/trouble.nvim',
   event = 'VeryLazy',
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
   cmd = 'Trouble',
   keys = {
     {
       '<leader>d',
       '<cmd>Trouble diagnostics toggle<cr>',
       desc = 'Fancy diagnostics',
-    },
-    {
-      '<leader>n',
-      '<cmd>Trouble symbols toggle focus=false<cr>',
-      desc = '[N]avigate module',
     },
   },
   opts = {

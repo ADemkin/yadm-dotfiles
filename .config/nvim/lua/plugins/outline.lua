9615c4ca1fafcc48ef1def3b21a56d368727296a
Anton Demkin <antondemkin@yandex.ru>
2025-08-05 16:13:20 +0200

    NVIM: Use Outline for code navigation

---
+++ .config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,33 @@
+return {
+  'hedyhli/outline.nvim',
+  event = 'VeryLazy',
+  config = function()
+    vim.keymap.set('n', '<leader>n', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+    require('outline').setup({
+      outline_window = {
+        auto_jump = true,
+      },
+      symbols = {
+        icons = {
+          Class = { icon = 'class', hl = 'Type' },
+          Method = { icon = 'meth', hl = 'Function' },
+          Constructor = { icon = 'init', hl = 'Special' },
+          Enum = { icon = 'Enum', hl = 'Type' },
+          Interface = { icon = 'I', hl = 'Type' },
+          Function = { icon = 'fn', hl = 'Function' },
+          Variable = { icon = 'var', hl = 'Constant' },
+          Constant = { icon = 'const', hl = 'Constant' },
+          String = { icon = 'str', hl = 'String' },
+          Boolean = { icon = 'bool', hl = 'Boolean' },
+          Array = { icon = '[]', hl = 'Constant' },
+          Object = { icon = 'obj', hl = 'Type' },
+          Key = { icon = 'k', hl = 'Type' },
+          Null = { icon = 'nil', hl = 'Type' },
+          EnumMember = { icon = 'enum', hl = 'Identifier' },
+          Struct = { icon = 'struct', hl = 'Structure' },
+          Operator = { icon = '+', hl = 'Identifier' },
+        },
+      },
+    })
+  end,
+}

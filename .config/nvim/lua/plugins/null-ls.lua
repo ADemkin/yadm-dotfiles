23973522a1fe01680f3d97546a725e7224fde723
Anton Demkin <antondemkin@yandex.ru>
2025-08-13 13:06:38 +0200

    NVIM: Remove cspell from nvim

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -7,30 +7,17 @@ return {
     'davidmh/cspell.nvim',
   },
   config = function(_, opts)
-    local cspell = require('cspell')
-    opts.sources = opts.sources or {}
-    table.insert(
-      opts.sources,
-      cspell.diagnostics.with({
-        diagnostics_postprocess = function(diagnostic)
-          diagnostic.severity = vim.diagnostic.severity.HINT
-        end,
-      })
-    )
-    table.insert(opts.sources, cspell.code_actions)
-
     local null_ls = require('null-ls')
-    local formatting = null_ls.builtins.formatting   -- to setup formatters
+    local formatting = null_ls.builtins.formatting -- to setup formatters
     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-    local code_actions = null_ls.builtins.code_actions
 
     -- Formatters & linters for mason to install
     require('mason-null-ls').setup({
       ensure_installed = {
-        'prettier',  -- ts/js formatter
-        'stylua',    -- lua formatter
-        'eslint_d',  -- ts/js linter
-        'shfmt',     -- Shell formatter
+        'prettier', -- ts/js formatter
+        'stylua', -- lua formatter
+        'eslint_d', -- ts/js linter
+        'shfmt', -- Shell formatter
         'checkmake', -- linter for Makefiles
         'gofmt',
         'ruff',
@@ -47,8 +34,6 @@ return {
       formatting.gofmt,
       formatting.goimports,
       formatting.golines,
-      diagnostics.cspell,
-      code_actions.cspell,
     }
 
     local augroup = vim.api.nvim_create_augroup('LspFormatting', {})

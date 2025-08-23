221bbed2ef1a00889dc540d41cb58812669cbf62
Anton Demkin <antondemkin@yandex.ru>
2025-08-23 19:06:05 +0200

    NVIM: Allow to run module tests in terminal

--- .config/nvim/lua/core/terminal.lua
+++ .config/nvim/lua/core/terminal.lua
@@ -5,7 +5,6 @@ local function get_function_name()
   while node do
     local node_type = node:type()
 
-    -- Try common function-like node types
     if
       node_type == 'function_definition' -- Python
       or node_type == 'function_declaration' -- Lua, JS
@@ -17,29 +16,62 @@ local function get_function_name()
         return vim.treesitter.get_node_text(name_node, 0)
       end
     end
-    node = node:parent()
+    if node_type == 'decorated_definition' then
+      node = node:field('definition')[1]
+    else
+      node = node:parent()
+    end
   end
   return nil
 end
 
-vim.keymap.set('n', '<leader>tt', function()
-  local name = get_function_name()
-  if name == nil then
-    return
+local function get_module_name_and_path()
+  local name = vim.api.nvim_buf_get_name(0)
+  return vim.fn.fnamemodify(name, ':t'), vim.fn.fnamemodify(name, ':.')
+end
+
+local function build_pytest_command(module_path, function_name)
+  local command = 'pytest'
+  if module_path ~= nil then
+    command = command .. ' ' .. module_path
   end
-  local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':.')
-  local command = ''
-  if vim.bo.filetype == 'python' then
-    if vim.startswith(name, 'test_') then
-      command = 'pytest ' .. path .. ' -k ' .. name
-    end
-  else
-    print('Filetype not supported: ' .. vim.bo.filetype)
-    return
+  if function_name ~= nil then
+    command = command .. ' -k ' .. function_name
   end
-  if command == '' then
-    print('Test not found')
-    return
-  end
+  return command
+end
+
+local function execute_in_terminal(command)
   vim.cmd('TermExec cmd="' .. command .. '"')
+end
+
+vim.keymap.set('n', '<leader>tt', function()
+  if not vim.bo.filetype == 'python' then
+    return
+  end
+  local module_name, module_path = get_module_name_and_path()
+  if not vim.startswith(module_name, 'test_') then
+    print('Module is not a test: ' .. module_name)
+    return
+  end
+  local function_name = get_function_name()
+  if function_name == nil then
+    print('Function name not found')
+    return
+  end
+  local command = build_pytest_command(module_path, function_name)
+  execute_in_terminal(command)
 end, { noremap = true, silent = true, desc = 'Run current pytest test in terminal' })
+
+vim.keymap.set('n', '<leader>tf', function()
+  if not vim.bo.filetype == 'python' then
+    return
+  end
+  local module_name, module_path = get_module_name_and_path()
+  if not vim.startswith(module_name, 'test_') then
+    print('Module is not a test: ' .. module_name)
+    return
+  end
+  local command = build_pytest_command(module_path)
+  execute_in_terminal(command)
+end, { noremap = true, silent = true, desc = 'Run current pytest module tests in terminal' })

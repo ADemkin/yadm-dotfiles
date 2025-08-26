ea0de40cd67e4688863a4c6c4549d0e8e8a77ec7
Anton Demkin <antondemkin@yandex.ru>
2025-08-26 10:08:14 +0200

    NVIM: Enable more features from flash

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -6,6 +6,9 @@ return {
       char = {
         enabled = false,
       },
+      search = {
+        highlight = { backdrop = true },
+      },
     },
     search = {
       multi_window = false,
@@ -21,6 +24,39 @@ return {
       function()
         require('flash').jump()
       end,
+      desc = 'Flash',
+    },
+    {
+      'S',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Remote Flash',
+    },
+    {
+      'R',
+      mode = { 'o', 'x' },
+      function()
+        require('flash').treesitter_search()
+      end,
+      desc = 'Treesitter Search',
+    },
+    {
+      '<c-s>',
+      mode = { 'c' },
+      function()
+        require('flash').toggle()
+      end,
+      desc = 'Toggle Flash Search',
     },
   },
 }

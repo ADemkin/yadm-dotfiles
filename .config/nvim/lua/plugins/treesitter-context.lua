8afa7f5b4a80ef3d1e5603d2aed9928db2aa0300
Anton Demkin <demkin.anton@rwb.ru>
2025-09-05 21:04:43 +0200

    NVIM: Fix tresitter event

--- .config/nvim/lua/plugins/treesitter-context.lua
+++ .config/nvim/lua/plugins/treesitter-context.lua
@@ -5,6 +5,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
   },
   config = function()
+    --- @param options? TSContext.UserConfig
     require('treesitter-context').setup({
       enable = true,
       max_lines = 2,
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPost', 'BufNewFile' },
+  event = { 'BufReadPre', 'BufNewFile' },
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`

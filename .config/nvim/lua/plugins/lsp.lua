f3bf41dc388d4a0af9ac9f3d468cc53b997a5cd5
Anton Demkin <antondemkin@yandex.ru>
2025-09-05 21:39:57 +0200

    NVIM: Fix no ft detected error

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,5 @@
 return {
   'neovim/nvim-lspconfig',
-  event = { 'BufReadPost', 'BufNewFile' },
   dependencies = {
     { 'mason-org/mason.nvim', opts = {} },
     'mason-org/mason-lspconfig.nvim',

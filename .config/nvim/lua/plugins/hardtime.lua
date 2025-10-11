8542721b46d214c64cccda1516947315ad014844
Anton Demkin <demkin.anton@rwb.ru>
2025-10-11 22:57:51 +0200

    NVIM: Run hardtime outside of working projects

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,5 +1,23 @@
 return {
   'm4xshen/hardtime.nvim',
   lazy = false,
-  opts = {},
+  config = function()
+    local function is_enabled(stopwords, cwd)
+      for _, word in ipairs(stopwords) do
+        if string.find(cwd, word) then
+          return false
+        end
+      end
+      return true
+    end
+    local stopwords = {
+      'moderation',
+    }
+    local cwd = vim.fn.getcwd()
+    local enabled = is_enabled(stopwords, cwd)
+    vim.notify('hardtime is ' .. tostring(enabled) .. ' for ' .. cwd)
+    require('hardtime').setup({
+      enabled = enabled,
+    })
+  end,
 }

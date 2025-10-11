4ebe459d90eeb4d8763029530838e1c824241ab3
Anton Demkin <demkin.anton@rwb.ru>
2025-10-11 22:56:09 +0200

    NVIM: Disable snippets and ensure buffer completion

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -4,23 +4,23 @@ return {
   version = '1.*',
   dependencies = {
     -- Snippet Engine
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = 'make install_jsregexp',
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
-      },
-      opts = {},
-    },
+    -- {
+    --   'L3MON4D3/LuaSnip',
+    --   version = '2.*',
+    --   build = 'make install_jsregexp',
+    --   dependencies = {
+    --     -- `friendly-snippets` contains a variety of premade snippets.
+    --     --    See the README about individual language/framework/plugin snippets:
+    --     --    https://github.com/rafamadriz/friendly-snippets
+    --     -- {
+    --     --   'rafamadriz/friendly-snippets',
+    --     --   config = function()
+    --     --     require('luasnip.loaders.from_vscode').lazy_load()
+    --     --   end,
+    --     -- },
+    --   },
+    --   opts = {},
+    -- },
     'giuxtaposition/blink-cmp-copilot',
     'folke/lazydev.nvim', -- required as a provider
   },
@@ -39,7 +39,14 @@ return {
       ghost_text = { enabled = false },
     },
     sources = {
-      default = { 'lsp', 'buffer', 'path', 'snippets', 'lazydev', 'copilot' },
+      default = {
+        'lsp',
+        'buffer',
+        'path',
+        -- 'snippets',
+        'lazydev',
+        'copilot',
+      },
       providers = {
         lazydev = {
           module = 'lazydev.integrations.blink',
@@ -50,6 +57,10 @@ return {
           module = 'blink-cmp-copilot',
           score_offset = 100,
           async = true,
+        },
+        buffer = {
+          module = 'blink.cmp.sources.buffer',
+          score_offset = -100,
         },
       },
     },

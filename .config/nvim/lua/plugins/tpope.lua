e29566b73e715b8487833bef60750240ac299574
Anton Demkin <demkin.anton@rwb.ru>
2025-10-11 14:09:08 +0200

    NVIM: Update gitgutter maps

--- .config/nvim/lua/plugins/tpope.lua
+++ .config/nvim/lua/plugins/tpope.lua
@@ -5,6 +5,7 @@ return {
     event = 'VeryLazy',
     config = function()
       vim.keymap.set('n', 'ts', ':Git<CR>', { noremap = true, silent = true, desc = 'Git status' })
+      vim.keymap.set('n', 'tg', ':Git<CR>', { noremap = true, silent = true, desc = 'Git status' })
     end,
   },
   { 'tpope/vim-commentary' },

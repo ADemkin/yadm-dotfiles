554ba563f14b89e27f0f71a5159f53301eea5bf4
demkin.anton <demkin.anton@rwb.ru>
2025-10-18 11:15:43 +0200

    NVIM: Make hardtime notification more subtle

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -2,7 +2,8 @@ return {
   'm4xshen/hardtime.nvim',
   lazy = false,
   config = function()
-    local function is_enabled(stopwords, cwd)
+    local function is_enabled(stopwords)
+      local cwd = vim.fn.getcwd()
       for _, word in ipairs(stopwords) do
         if string.find(cwd, word) then
           return false
@@ -13,9 +14,9 @@ return {
     local stopwords = {
       'moderation',
     }
-    local cwd = vim.fn.getcwd()
-    local enabled = is_enabled(stopwords, cwd)
-    vim.notify('hardtime is ' .. tostring(enabled) .. ' for ' .. cwd)
+    local enabled = is_enabled(stopwords)
+    local enabled_str = is_enabled and 'enabled' or 'disabled'
+    vim.notify('hardtime is ' .. enabled_str)
     require('hardtime').setup({
       enabled = enabled,
     })

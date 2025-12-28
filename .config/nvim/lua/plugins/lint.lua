309fe2969474ea5a2354efb23766494e214d1798
demkin.anton <demkin.anton@rwb.ru>
2025-12-28 13:08:33 +0200

    NVIM: Remove unused lint code

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -26,35 +26,11 @@ return {
         end,
       })
 
-      -- vim.diagnostic.enable(false)
-
       local function toggle_diagnostics()
         local is_enabled = vim.diagnostic.is_enabled()
         vim.diagnostic.enable(not is_enabled)
         vim.notify('Diagnistics ' .. (not is_enabled and '1' or '0'), vim.log.levels.DEBUG)
       end
-
-      -- TODO: this conflicts with after/lsp/basedpyright.lua but working
-      -- local function toggle_pyright_type_checking_mode()
-      --   local client = vim.lsp.get_clients({ name = 'basedpyright' })[1]
-      --   if not client then
-      --     vim.notify('BasedPyright LSP is not active', vim.log.levels.WARN)
-      --     return
-      --   end
-      --   local analysis = client.config.settings.basedpyright.analysis
-      --   vim.notify('BasedPyright typeCheckingMode: ' .. analysis.typeCheckingMode, vim.log.levels.DEBUG)
-      --   if analysis.typeCheckingMode == 'basic' then
-      --     analysis.typeCheckingMode = 'recommended'
-      --   elseif analysis.typeCheckingMode == 'recommended' then
-      --     analysis.typeCheckingMode = 'basic'
-      --   end
-      --   vim.lsp.stop_client(client.id)
-      --   vim.defer_fn(function()
-      --     vim.cmd('LspStart basedpyright')
-      --     vim.notify('BasedPyright restarted with typeCheckingMode: ' .. analysis.typeCheckingMode)
-      --   end, 100)
-      -- end
-      -- vim.keymap.set('n', '<leader>p', toggle_pyright_type_checking_mode)
 
       vim.keymap.set('n', 'gtd', toggle_diagnostics)
       vim.keymap.set('n', '<leader>l', toggle_diagnostics)

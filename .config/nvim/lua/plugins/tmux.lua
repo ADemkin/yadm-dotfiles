eebeec4c1373f0590d4aaac8d23cb33bf00b33fa
demkin.anton <demkin.anton@rwb.ru>
2026-02-05 18:48:12 +0200

    NVIM: Disallow to move out of zoomed TMUX pane

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -5,6 +5,7 @@ return {
     return require('tmux').setup({
       navigation = {
         enable_default_keybindings = true,
+        persist_zoom = true,
       },
       resize = {
         enable_default_keybindings = true,

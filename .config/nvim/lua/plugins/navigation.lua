bdc07aae550d40dd9653a4f0784bf9e78742ee35
demkin.anton <demkin.anton@rwb.ru>
2026-02-14 15:12:19 +0200

    NVIM: Update code navigation

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -1,13 +1,21 @@
 return {
+  -- aerial has telescope integration!
   -- Also consider aerial.nvim and oskarrrrrrr/symbols.nvim
   {
     'hedyhli/outline.nvim',
     lazy = true,
     cmd = { 'Outline', 'OutlineOpen' },
     opts = {
-      -- outline_window = {
-      --   auto_jump = true,
-      -- },
+      outline_window = {
+        position = 'left',
+        width = 15,
+        auto_width = {
+          enabled = true,
+          max_width = 40,
+        },
+        auto_close = true,
+        auto_jump = true,
+      },
       symbols = {
         icons = {
           Class = { icon = 'C', hl = 'Type' },

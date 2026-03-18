d5de550d2ce498e8c335fb4504e78c5f309a3799
demkin.anton <demkin.anton@rwb.ru>
2026-03-18 22:33:37 +0200

    NVIM: Cleanup treesitter context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,5 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  -- dependencies = {
-  --   'nvim-treesitter/nvim-treesitter-context',
-  -- },
   event = { 'BufReadPre', 'BufNewFile' },
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts

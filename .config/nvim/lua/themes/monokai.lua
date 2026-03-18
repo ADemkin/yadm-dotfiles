fa908d7488cee74311d2eff3533e806a80d785de
demkin.anton <demkin.anton@rwb.ru>
2026-03-18 14:46:10 +0200

    NVIM: Finetune monokai

--- .config/nvim/lua/themes/monokai.lua
+++ .config/nvim/lua/themes/monokai.lua
@@ -1,25 +1,66 @@
+function dumps(data)
+  local raw = vim.fn.json_encode(data)
+  ok, _ = pcall(vim.fn.writefile, { raw }, './monokai_scheme.json')
+  vim.print(ok)
+end
 return {
   'loctvl842/monokai-pro.nvim',
   lazy = false,
   priority = 1000,
   config = function()
     require('monokai-pro').setup({
+      -- TODO: FIX FUCKING SEARCH HIGHLIGHTS!!!
+      -- TODO: FIX FUCKING DICTIONARY ERRORS IN RED AND UNDERCURL!
+      -- TODO: Fix complement bracket highlight (like in gruvbox?)
       -- TODO: f"" make f orange
       -- TODO: @decorator make @ red
+      -- TODO: fix black color in lualine
+      -- TODO: fix error suggestion: border and bg
       filter = 'classic', -- classic | octagon | pro | machine | ristretto | spectrum
+      ---@field override? fun(scheme: MonokaiPro.Scheme)
       override = function(scheme)
+        -- dumps(scheme)
+        local white = { fg = scheme.base.white }
+        local orange = { fg = scheme.base.blue } -- yes, orange is blue
+        local aqua = { fg = scheme.base.cyan }
+        local green = { fg = scheme.base.green }
+        local yellow = { fg = scheme.base.yellow }
+        local purple = { fg = scheme.base.magenta }
+        local red = { fg = scheme.base.red }
         return {
-          ['@punctuation.bracket'] = { fg = scheme.base.white },
-          ['@constructor'] = { fg = scheme.base.white },
-          ['@constant'] = { fg = scheme.base.white },
-          ['@function.method.call'] = { fg = scheme.base.cyan },
-          ['@variable.parameter'] = { fg = scheme.base.white },
-          ['@type'] = { fg = scheme.base.green },
-          ['@lsp.typemod.selfParameter.parameter.python'] = { fg = scheme.base.blue },
-          ['@lsp.typemod.clsParameter.parameter.python'] = { fg = scheme.base.blue },
-          ['@punctuation.special'] = { fg = scheme.base.blue },
-          ['@punctuation.delimiter'] = { fg = scheme.base.white },
-          ['@lsp.typemod.parameter.parameter.python'] = { fg = scheme.base.blue },
+          WinSeparator = { fg = scheme.base.dimmed4 },
+          ['@punctuation.bracket'] = white,
+          ['@constructor'] = white,
+          ['@constant'] = white,
+          ['@function.method.call'] = aqua,
+          ['@function.builtin'] = aqua,
+          ['@variable.parameter'] = white,
+          ['@module'] = white,
+          ['@constant.builtin'] = aqua,
+          -- ['@type'] = green,
+          ['@type'] = white,
+          ['@punctuation.special'] = red,
+          ['@comment.todo.comment'] = orange,
+          -- ['@punctuation.special'] = orange,
+          ['@punctuation.delimiter'] = white,
+          ['@string.documentation'] = yellow,
+          -- python
+          ['@lsp.typemod.selfParameter.parameter.python'] = orange,
+          ['@lsp.typemod.clsParameter.parameter.python'] = orange,
+          ['@lsp.typemod.parameter.parameter.python'] = orange,
+          ['@lsp.typemod.class.declaration.python'] = green,
+          -- ['@type.python'] = white,
+          -- ['@lsp.type.class.python'] = white,
+          -- lua
+          ['@comment.documentation.lua'] = white,
+          -- git
+          ['@markup.heading.gitcommit'] = white,
+          -- ['@string.special.path.gitcommit'] = { link = 'Comment' },
+          ['@string.special.path.gitcommit'] = yellow,
+          ['fugitiveSymbolicRef'] = white,
+          ['fugitiveUntrackedModifier'] = orange,
+          -- yaml
+          ['@property.yaml'] = white,
         }
       end,
       ---@field override_scheme? fun(scheme: MonokaiPro.Scheme, palette: MonokaiPro.Palette, colors: MonokaiPro.Colors): MonokaiPro.Scheme
@@ -27,11 +68,6 @@ return {
         scheme.breadcrumb.foreground = scheme.base.white
         -- scheme.statusBar.background = scheme.base.black
         return scheme
-        -- return {
-        --   statusBar = {
-        --     background = scheme.base.white,
-        --   },
-        -- }
       end,
     })
     vim.cmd.colorscheme('monokai-pro')

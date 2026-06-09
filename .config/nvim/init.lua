2d204b26f06334bbfc5c121791c99102831ea414
demkin.anton <demkin.anton@rwb.ru>
2026-06-09 17:35:54 +0200

    NVIM: Remove unused diff plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,15 +6,17 @@ require('core.events')
 require('core.spell')
 
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+-- local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+-- if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+--   local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+--   if vim.v.shell_error ~= 0 then
+--     error('Error cloning lazy.nvim:\n' .. out)
+--   end
+-- end
+-- vim.opt.rtp:prepend(lazypath)
+
+vim.pack.add({ 'https://github.com/folke/lazy.nvim.git' }, { confirm = false })
 
 require('lazy').setup({
   -- Appearance
@@ -94,17 +96,6 @@ require('lazy').setup({
         time_format = '24h',
       })
     end,
-  },
-
-  -- Zed like diff ?
-  {
-    'martindur/zdiff.nvim',
-    cmd = 'Zdiff',
-    keys = {
-      { '<leader>zd', '<cmd>Zdiff<cr>', desc = 'Zdiff (uncommitted)' },
-      { '<leader>zD', '<cmd>Zdiff main<cr>', desc = 'Zdiff (vs main)' },
-    },
-    opts = {},
   },
 }, {
   dev = { path = '~/code' },

8717905b495ec185e4235f7aa85ace80340226db
demkin.anton <demkin.anton@rwb.ru>
2026-07-21 20:28:46 +0200

    NVIM: Fix refactoring deps

--- .config/nvim/lua/plugins/refactoring.lua
+++ .config/nvim/lua/plugins/refactoring.lua
@@ -3,8 +3,7 @@ return {
   {
     'ThePrimeagen/refactoring.nvim',
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
+      'lewis6991/async.nvim',
     },
     lazy = true,
     opts = {},

8b5d4f794b3ad2a665535149adcce05a8197c643
demkin.anton <demkin.anton@rwb.ru>
2026-08-02 07:59:38 +0200

    NVIM: Disable auto brackets on function completion

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -41,6 +41,7 @@ return {
     completion = {
       documentation = { auto_show = true, auto_show_delay_ms = 500 },
       ghost_text = { enabled = false },
+      accept = { auto_brackets = { enabled = false } },
     },
     sources = {
       default = {
